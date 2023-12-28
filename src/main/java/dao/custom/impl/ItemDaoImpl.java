cb85266bb49be3b064792229c9650c80ed7e8b82
Hansajaa <nilanahansaja21@gmail.com>
2023-12-28 18:46:12 +0530

    Item form complete with Hibernate

--- src/main/java/dao/custom/impl/ItemDaoImpl.java
+++ src/main/java/dao/custom/impl/ItemDaoImpl.java
@@ -4,7 +4,11 @@ import DB.DBConnection;
 import Dto.ItemDto;
 import dao.custom.ItemDao;
 import dao.util.CrudUtil;
+import dao.util.HibernateUtil;
 import entity.Item;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,7 +21,14 @@ public class ItemDaoImpl implements ItemDao {
     @Override
     public boolean save(Item entity) throws SQLException, ClassNotFoundException {
 
-        String sql = "INSERT INTO item VALUES(?,?,?,?)";
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(entity);
+        transaction.commit();
+        session.close();
+        return true;
+
+//        String sql = "INSERT INTO item VALUES(?,?,?,?)";
 
 //        PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
 //        pstm.setString(1, entity.getCode());
@@ -27,40 +38,66 @@ public class ItemDaoImpl implements ItemDao {
 //
 //        return pstm.executeUpdate()>0;
 
-        return CrudUtil.execute(sql,entity.getCode(),entity.getDescription(),entity.getUnitPrice(),entity.getQtyOnHand());
+//        return CrudUtil.execute(sql,entity.getCode(),entity.getDescription(),entity.getUnitPrice(),entity.getQtyOnHand());
 
     }
 
     @Override
     public boolean update(Item entity) throws SQLException, ClassNotFoundException {
-        String sql = "UPDATE item set description=?, unitPrice=?, qtyOnHand=? WHERE code=?";
+//        String sql = "UPDATE item set description=?, unitPrice=?, qtyOnHand=? WHERE code=?";
+//
+//        return CrudUtil.execute(sql,entity.getDescription(),entity.getUnitPrice(),entity.getQtyOnHand(),entity.getCode());
 
-        return CrudUtil.execute(sql,entity.getDescription(),entity.getUnitPrice(),entity.getQtyOnHand(),entity.getCode());
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Item item = session.find(Item.class, entity.getCode());
+        item.setDescription(entity.getDescription());
+        item.setQtyOnHand(entity.getQtyOnHand());
+        item.setUnitPrice(entity.getUnitPrice());
+        session.save(item);
+
+        transaction.commit();
+        session.close();
+
+        return true;
 
     }
 
     @Override
     public boolean delete(String value) throws SQLException, ClassNotFoundException {
-        String sql = "DELETE FROM item WHERE code = ?";
-//        Statement stm = DBConnection.getInstance().getConnection().createStatement();
-//
-//        return stm.executeUpdate(sql)>0;
-        return CrudUtil.execute(sql,value);
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(session.find(Item.class,value));
+        transaction.commit();
+        session.close();
+
+        return true;
+
     }
 
     @Override
     public List<Item> getAll() throws SQLException, ClassNotFoundException {
-
-        List<Item> itemList = new ArrayList<>();
-        String sql="SELECT * FROM item";
+//
+//        List<Item> itemList = new ArrayList<>();
+//        String sql="SELECT * FROM item";
 //        Statement stm = DBConnection.getInstance().getConnection().createStatement();
-        ResultSet resultSet = CrudUtil.execute(sql);
-        while (resultSet.next()){
-            Item item=new Item(resultSet.getString(1),resultSet.getString(2),resultSet.getDouble(3),resultSet.getInt(4));
-            itemList.add(item);
-        }
+//        ResultSet resultSet = CrudUtil.execute(sql);
+//        while (resultSet.next()){
+//            Item item=new Item(resultSet.getString(1),resultSet.getString(2),resultSet.getDouble(3),resultSet.getInt(4));
+//            itemList.add(item);
 
-        return itemList;
+
+
+//        }
+
+//        return itemList;
+
+        Session session = HibernateUtil.getSession();
+        Query query = session.createQuery("FROM Item ");
+        List<Item> list = query.list();
+
+        return list;
     }
 //
 //    @Override
